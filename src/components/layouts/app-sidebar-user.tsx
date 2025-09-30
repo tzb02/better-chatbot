c1206233a08e2f0914888566c4687f22d2e97a35
cgoing <neo.cgoing@gmail.com>
2025-09-30 17:24:30 +0900

    chore: latest user data

--- src/components/layouts/app-sidebar-user.tsx
+++ src/components/layouts/app-sidebar-user.tsx
@@ -49,7 +49,9 @@ export function AppSidebarUserInner(props: {
   const { data: user } = useSWR<BasicUser>(`/api/user/details`, fetcher, {
     fallbackData: props.user,
     suspense: true,
-    revalidateOnMount: false,
+    revalidateOnFocus: false,
+    shouldRetryOnError: false,
+    refreshInterval: 1000 * 60 * 10,
   });
   const appStoreMutate = appStore((state) => state.mutate);
   const t = useTranslations("Layout");
